eab00c1b09c040b6f554402489fc31f2d74dff3c
= <maeva.pasteur.pro@gmail.com>
2021-05-13 17:33:40 +0200

    [feat]: add views

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {
 
 import './assets/scss/style.scss'
 import Home from './views/Home';
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Profile from "./views/Profile";
 import Layout from "./layouts/Layout";
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
             <Layout>
                 <Switch>
                     <Route path="/" exact strict sensitive component={Home}/>
+                    <Route path="/inscription" exact strict sensitive component={Register}/>
+                    <Route path="/connexion" exact strict sensitive component={Login}/>
+                    <Route path="/mon-compte" exact strict sensitive component={Profile}/>
                 </Switch>
             </Layout>
         </Router>
---
+++ src/views/Profile.jsx
@@ -0,0 +1,9 @@
+function Profile() {
+    return (
+        <section>
+            <h1>Inscription</h1>
+        </section>
+    )
+}
+
+export default Profile;
