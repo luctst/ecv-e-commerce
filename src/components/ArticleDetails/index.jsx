687fa3deb2c291d7de6aab3989ff443e2bc2e6f5
= <maeva.pasteur.pro@gmail.com>
2021-05-13 22:57:25 +0200

    [style]: article details

--- src/components/ArticleDetails/index.jsx
+++ src/components/ArticleDetails/index.jsx
@@ -1,16 +1,29 @@
 import './style.scss';
 import LazyImage from "../LazyImage";
 import { Link } from "react-router-dom";
+import LinkButton from "../LinkButton";
+
+const article = {
+    id: 1,
+    handle: 'chemise-a-fleur',
+    title: "Chemise à fleur",
+    brand: "Levi's",
+    price: "79.00",
+    image: "https://medias-cache.citadium.com/fr/levis-chemise-bleu/image/35/8/3025358.426.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci asperiores beatae dignissimos distinctio facere magni mollitia nulla officiis.",
+    category: "Chemise"
+};
 
 function ArticleDetails() {
     return (
         <section className="article-details">
-            <LazyImage src="https://streetartnews.net/wp-content/themes/contemporaryartnews/assets/images/4.4d7a51ab.jpg" alt="Titre de l'article"/>
+            <LazyImage src={article.image} alt={article.title}/>
             <div>
-                <Link to="/" class="category">Actualité</Link>
-                <p className="date">20/02/2021</p>
-                <h1>Article</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci asperiores beatae dignissimos distinctio facere magni mollitia nulla officiis, optio, quaerat quasi qui recusandae repellendus rerum sequi soluta tempora totam.</p>
+                <LinkButton link="/articles">{article.category}</LinkButton>
+                <p className="brand">{article.brand}</p>
+                <h1>{article.title}</h1>
+                <p className="price">{article.price}€</p>
+                <p>{article.description}</p>
             </div>
         </section>
     )
