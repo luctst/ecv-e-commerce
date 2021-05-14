aa1898e8ad137240108ab18949f3fcfaa1fb0652
= <maeva.pasteur.pro@gmail.com>
2021-05-15 00:02:01 +0200

    [style]: search popin

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,14 @@ import './style.scss';
 import logo from '../../assets/images/logo.svg';
 import search from '../../assets/images/icons/search.svg';
 import { Link } from "react-router-dom";
+import Input from "../Input";
+import Button from "../Button";
+import { useState } from "react";
 
 function Header() {
+
+    const [open, setOpen] = useState(false);
+
     return (
         <header>
             <nav>
@@ -26,12 +32,18 @@ function Header() {
                         <Link to="/mon-compte">Mon compte</Link>
                     </li>
                     <li>
-                        <Link to="/">
+                        <button onClick={() => setOpen(!open)}>
                             <img src={search} alt="Faire une recherche"/>
-                        </Link>
+                        </button>
                     </li>
                 </ul>
             </nav>
+            {open && (
+                <form className="search">
+                    <Input type="text" required={true}>Rechercher</Input>
+                    <Button type="submit">OK</Button>
+                </form>
+            )}
         </header>
     );
 }
